378422868d736f885c55e7d0199b113aaee0795f
g-rmc <g.rmc3000@gmail.com>
2022-08-25 23:11:25 -0300

    Configuração da request POST de usuários

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const users = [{   
+    username: 'bobesponja',
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
+}]
+
+const tweets = [{   
+    username: "bobesponja",
+    tweet: "eu amo o hub"
+}]
+
+app.post('/sign-up', (req, res) => {
+    users.push(req.body);
+    res.send('OK');
+})
+
+app.listen('5000', () => console.log('Listening on 5000'));
