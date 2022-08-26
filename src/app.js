3516305368fc586c3e7d622fcbc758d55ab3e510
g-rmc <g.rmc3000@gmail.com>
2022-08-25 23:41:21 -0300

    Conclusão dos requisitos base

--- src/app.js
+++ src/app.js
@@ -8,38 +8,38 @@ app.use(express.json());
 const users = [{   
     username: "bobesponja",
     avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
-}]
+}];
 
 const tweets = [{   
     username: "bobesponja",
     tweet: "eu amo o hub"
-}]
+}];
 
 app.post('/sign-up', (req, res) => {
     users.push(req.body);
     res.send('OK');
-})
+});
 
 app.post('/tweets', (req, res) => {
     tweets.push(req.body);
     res.send('OK');
-})
+});
 
 app.get('/tweets', (req, res) => {
     let last10 = getTweets(0,9);
     last10 = last10.map(addAvatar);
     res.send(last10);
-})
+});
 
 function getTweets(from, to) {
     const arr = tweets.filter((value, index) => index <= tweets.length-from && index >= tweets.length-from-to);
     return arr
-}
+};
 
 function addAvatar(value) {
     const user = users.find(element => element.username === value.username);
     value.avatar = user.avatar;
     return value
-}
+};
 
 app.listen('5000', () => console.log('Listening on 5000'));
