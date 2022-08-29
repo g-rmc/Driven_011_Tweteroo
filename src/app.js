592be13fd1c31e7dcf4ea7fecfafa3a72baa0e32
g-rmc <g.rmc3000@gmail.com>
2022-08-29 14:14:42 -0300

    Bug corrigido: getTweets atualizada para pegar o número correto de tweets

--- src/app.js
+++ src/app.js
@@ -11,11 +11,144 @@ const users = [{
     avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
 }];
 
-const tweets = [{
+/* const tweets = [{
     id: 0,  
     username: "bobesponja",
     tweet: "eu amo o hub"
-}];
+}]; */
+
+const tweets = [
+    {
+        id: 0,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 1,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 2,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 3,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 4,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 5,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 6,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 7,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 8,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 9,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 10,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 11,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 12,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 13,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 14,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 15,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 16,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 17,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 18,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 19,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 20,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 21,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 22,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 23,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 24,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    },
+    {
+        id: 25,  
+        username: "bobesponja",
+        tweet: "eu amo o hub"
+    }
+]
 
 app.post('/sign-up', (req, res) => {
     const { username, avatar} = req.body;
@@ -67,7 +200,7 @@ app.get('/tweets', (req, res) => {
     }
     let tweetsList = getTweets(req.query.page);
     tweetsList = tweetsList.map(addAvatar);
-    res.send(tweetsList.reverse());
+    res.send(tweetsList);
 });
 
 app.get('/tweets/:USERNAME', (req, res) => {
@@ -79,9 +212,10 @@ app.get('/tweets/:USERNAME', (req, res) => {
 
 function getTweets(page) {
     const pageNum = Number(page)-1;
-    const from = 0 + 10*pageNum;
-    const to = 9 + 10*pageNum;
-    const arr = tweets.filter((value, index) => index <= tweets.length-from && index >= tweets.length-from-to);
+    const from = 0 + (10*pageNum);
+    const to = 9 + (10*pageNum);
+    const aux = [... tweets];
+    const arr = aux.reverse().filter((value, index) => index >= from && index <= to);
     return arr
 };
 
