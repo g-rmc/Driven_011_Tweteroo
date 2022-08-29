44e9e440737b799d742e85f949a26fe035500a77
g-rmc <g.rmc3000@gmail.com>
2022-08-29 17:17:11 -0300

    Ajustes no código para remover Code Smells

--- src/app.js
+++ src/app.js
@@ -11,144 +11,11 @@ const users = [{
     avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
 }];
 
-/* const tweets = [{
+const tweets = [{
     id: 0,  
     username: "bobesponja",
     tweet: "eu amo o hub"
-}]; */
-
-const tweets = [
-    {
-        id: 0,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 1,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 2,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 3,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 4,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 5,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 6,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 7,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 8,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 9,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 10,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 11,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 12,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 13,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 14,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 15,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 16,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 17,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 18,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 19,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 20,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 21,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 22,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 23,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 24,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    },
-    {
-        id: 25,  
-        username: "bobesponja",
-        tweet: "eu amo o hub"
-    }
-]
+}];
 
 app.post('/sign-up', (req, res) => {
     const { username, avatar} = req.body;
@@ -207,7 +74,8 @@ app.get('/tweets/:USERNAME', (req, res) => {
     const username = req.params.USERNAME;
     let userTweets = tweets.filter(value => value.username === username);
     userTweets = userTweets.map(addAvatar);
-    res.send(userTweets.reverse());
+    userTweets.reverse();
+    res.send(userTweets);
 })
 
 function getTweets(page) {
@@ -215,7 +83,8 @@ function getTweets(page) {
     const from = 0 + (10*pageNum);
     const to = 9 + (10*pageNum);
     const aux = [... tweets];
-    const arr = aux.reverse().filter((value, index) => index >= from && index <= to);
+    aux.reverse();
+    const arr = aux.filter((value, index) => index >= from && index <= to);
     return arr
 };
 
